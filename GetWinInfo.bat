cd905de26bae921a04c416413847c8196101bc35
Stefano <carli.stefano@gmail.com>
2017-07-13 11:28:52 +0200

    Minor fixes. Different output.

--- GetWinInfo.bat
+++ GetWinInfo.bat
@@ -12,12 +12,20 @@ setlocal EnableDelayedExpansion
 
 TITLE Get Windows OS info
 
+set folder=%~dp0tester\
+
 REM Ask the user if she wants to proceed
 :choice
-echo This batch script will gather information of your Windows system and will generate the following files in the current directory:
+echo This batch script will gather information of your Windows system and will generate the following files in %folder%:
 echo - directories.txt
-echo - useraccounts.txt
-echo - networking.txt
+echo - tasklist.txt
+echo - network.txt
+echo - reg-(registry type).txt
+echo - antivirus.txt
+echo - users
+echo - hotfixes
+
+
 SET /P ANSWER=Do you want to proceed? (Y/N)
 if /I {%ANSWER%}=={Y} (goto :checkOS)
 if /I {%ANSWER%}=={N} (goto :END)
@@ -38,126 +46,147 @@ echo Please Wait....
 
 REM set variables
 set "volume=C:"
-set folder=tester
+
 
 REM Create directory if it doesn't exist
 IF NOT EXIST %folder% md %folder%
 
 REM List down all the directories 
 echo [*] Getting directories...
-SET file="%folder%/directories.txt"
+SET file="%folder%directories.txt"
 REM Simplified Directory listing
 echo -------------------------------------------- > %file%
+echo DIRECTORY LISTING >> %file%
+echo on Computer: %COMPUTERNAME% >> %file%
+echo Current Date and Time: %DATE% %TIME% >> %file%
+echo -------------------------------------------- >> %file%
 echo DIRECTORY LISTING (Simplified) >> %file%
 echo -------------------------------------------- >> %file%
 dir %volume%\ /b /a:d >> %file%
-echo [+] A SIMPLIFIED directory list has been written in the file %file%
+echo - Simplified list
 REM List all the directories
 echo -------------------------------------------- >> %file%
 echo DIRECTORY LISTING (Full)>> %file%
 echo -------------------------------------------- >> %file%
 dir %volume%\ /b /s /a:d >> %file%
-echo [+] A FULL directory list has been written in the file %file%
+echo - Full list
+echo done
 
 REM Get Networking information
-echo [*] Getting network info
-SET file="%folder%/network.txt"
-echo [+] Interfaces
+echo [*] Getting network info...
+SET file="%folder%network.txt"
+echo - Interfaces
+echo -------------------------------------------- > %file%
+echo NETWORKING INFO >> %file%
+echo on Computer: %COMPUTERNAME% >> %file%
+echo Current Date and Time: %DATE% %TIME% >> %file%
 echo -------------------------------------------- > %file%
 echo INTERFACES >> %file%
 echo -------------------------------------------- >> %file%
 ipconfig /all >> %file% 
 echo -------------------------------------------- >> %file%
-echo [+] Routes
+echo - Routes
 echo ROUTE >> %file%
 echo -------------------------------------------- >> %file%
 route print >> %file% 
 echo -------------------------------------------- >> %file%
-echo [+] ARP table
+echo - ARP table
 echo ARP TABLE >> %file%
 echo -------------------------------------------- >> %file%
 arp -a >> %file% 
 echo -------------------------------------------- >> %file%
+echo - Network connections
 echo [+]NETWORK CONNETIONS >> %file%
 echo -------------------------------------------- >> %file%
 netstat -ano >> %file% 
-echo [+] Networking information have been written in the file %file%
+echo done
 
 REM Get Tasklist 
-echo [*] Getting tasklist
-SET file="%folder%/tasklist.txt"
+echo | set /p="[*] Getting tasklist... "
+SET file="%folder%tasklist.txt"
 echo -------------------------------------------- > %file%
 echo TASKLIST >> %file%
+echo on Computer: %COMPUTERNAME% >> %file%
+echo Current Date and Time: %DATE% %TIME% >> %file%
 echo -------------------------------------------- >> %file%
 tasklist /V >> %file%
-echo -------------------------------------------- >> %file%
-echo [+] Tasklist information have been written in the file %file%
+echo done
 
 REM Getting registry info
-echo [*] Getting registry info
-reg export HKEY_CLASSES_ROOT "%folder%\reg-HKEY_CLASSES_ROOT.txt" /y
-echo [+] HKEY_CLASSES_ROOT information has been written in the file "%folder%\reg-HKEY_CLASSES_ROOT.txt"
-reg export HKEY_CURRENT_USER "%folder%\reg--HKEY_CURRENT_USER.txt" /y
-reg export HKEY_LOCAL_MACHINE "%folder%\reg-HKEY_LOCAL_MACHINE.txt" /y
-reg export HKEY_USERS "%folder%\reg-HKEY_USERS.txt" /y
-reg export HKEY_CURRENT_CONFIG "%folder%\reg-HKEY_CURRENT_CONFIG.txt" /y
+echo [*] Getting registry info. Please be patient...
+reg export HKEY_CLASSES_ROOT "%folder%reg-HKEY_CLASSES_ROOT.txt" /y >> nul
+echo - HKEY_CLASSES_ROOT
+reg export HKEY_CURRENT_USER "%folder%reg--HKEY_CURRENT_USER.txt" /y >> nul
+echo - HKEY_CURRENT_USER
+reg export HKEY_LOCAL_MACHINE "%folder%reg-HKEY_LOCAL_MACHINE.txt" /y >> nul
+echo - HKEY_LOCAL_MACHINE
+reg export HKEY_USERS "%folder%reg-HKEY_USERS.txt" /y >> nul
+echo - HKEY_USERS
+reg export HKEY_CURRENT_CONFIG "%folder%reg-HKEY_CURRENT_CONFIG.txt" /y >> nul
+echo - HKEY_CURRENT_CONFIG
+echo done
 
 REM Getting antivirus
-echo [*] Getting installed antivirus
-SET file="%folder%\antivirus.txt"
+echo | set /p="[*] Getting installed antivirus... "
+SET file="%folder%antivirus.txt"
 echo -------------------------------------------- > %file%
 echo ANTIVIRUS >> %file%
 echo on Computer: %COMPUTERNAME% >> %file%
 echo Current Date and Time: %DATE% %TIME% >> %file%
 echo -------------------------------------------- >> %file%
 wmic /APPEND:%file% /namespace:\\root\securitycenter2 path antivirusproduct >> nul
+echo done
 
 REM Getting usernames
-echo [*] Getting usernames
-SET file="%~dp0%folder%\users.txt"
+echo | set /p="[*] Getting local usernames... "
+SET file="%folder%users.txt"
 echo -------------------------------------------- > %file%
 echo USERS >> %file%
 echo on Computer: %COMPUTERNAME% >> %file%
 echo Current Date and Time: %DATE% %TIME% >> %file%
 echo -------------------------------------------- >> %file%
 wmic /APPEND:%file% UserAccount where "LocalAccount=True" >> nul
+echo done
 
 REM Getting hotfixes and service packs
-echo [*] Getting hotfixes and service packs. It may take a while...
-SET file="%~dp0%folder%\hotfixes.txt"
+echo | set /p="[*] Getting hotfixes and service packs. It may take a while... "
+SET file="%folder%hotfixes.txt"
 echo -------------------------------------------- > %file%
 echo HOTFIXES AND SERVICE PACKS >> %file%
 echo on Computer: %COMPUTERNAME% >> %file%
 echo Current Date and Time: %DATE% %TIME% >> %file%
 echo -------------------------------------------- >> %file%
 wmic /APPEND:%file% qfe >> nul
+echo done
 
 REM Getting startup list
-echo [*] Getting startup list
-SET file="%~dp0%folder%\startup.txt"
+echo | set /p="[*] Getting startup list... "
+SET file="%folder%startup.txt"
 echo -------------------------------------------- > %file%
 echo STARTUP LIST >> %file%
 echo on Computer: %COMPUTERNAME% >> %file%
 echo Current Date and Time: %DATE% %TIME% >> %file%
 echo -------------------------------------------- >> %file%
 wmic /APPEND:%file% startup list full >> nul
+echo done
 
 REM Getting current environment variable settings
-echo [*] Getting environment variables
-SET file="%~dp0%folder%\enviromen.txt"
+echo | set /p="[*] Getting environment variables... "
+SET file="%folder%environment.txt"
 echo -------------------------------------------- > %file%
 echo environment VARIABLE SETTINGS >> %file%
 echo on Computer: %COMPUTERNAME% >> %file%
 echo Current Date and Time: %DATE% %TIME% >> %file%
 echo -------------------------------------------- >> %file%
 set >> %file%
+echo done
 
 ::====================================================
 
 goto END
 
 :NOWIN
-echo Error...This is not Windows...
+echo Error...This is not Windows! How could this even be possible?
 goto END
 
 :END
